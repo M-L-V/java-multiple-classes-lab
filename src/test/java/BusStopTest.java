e6eb3da3d9a6331258d8011199e070acd19eda29
Morgaine <morgainedv@gmail.com>
2020-01-07 15:04:44 +0000

    adds bus stop set up to busstop class

---
+++ src/test/java/BusStopTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+
+public class BusStopTest {
+
+    private BusStop busStop;
+
+    @Before
+    public void before(){
+        busStop = new BusStop("Castle");
+    }
+
+    
+}
